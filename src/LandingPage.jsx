a64a331fe0074233f3b08b47ce7f896646bee00d
Atharav  Jadhav <atharav3233@gmail.com>
2024-05-29 20:37:51 +0530

    pagenotfound component added

--- src/LandingPage.jsx
+++ src/LandingPage.jsx
@@ -2,6 +2,7 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Home from "./Home"
 import Error from "./Error"
 import Help from './Help';
+import PageNotFound from './PageNotFound';
 
 
 
@@ -14,6 +15,7 @@ export default function () {
                     <Route path='/' element={<Home />} />
                     <Route path='/Error' element={<Error />} />
                     <Route path='/Help' element={<Help />} />
+                    <Route path='*' element={<PageNotFound />} />
                 </Routes>
             </Router>
             {/* 
---
+++ src/PageNotFound.jsx
@@ -0,0 +1,10 @@
+
+
+export default function PageNotFound() {
+    return (
+        <>
+            <h2>Invalid Page</h2>
+            <p>Path entered is invalid</p>
+        </>
+    )
+}
